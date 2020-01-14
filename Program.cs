8d34254b315bad2b336a5070ca66e98f8329e583
KristofferSaxmo <54435541+KristofferSaxmo@users.noreply.github.com>
2020-01-14 13:45:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antal nummer: ");
+            int antal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Högsta möjliga nummer: ");
+            int högst = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            int[] nummer = new int[antal];
+            for (int i = 0; i < antal; i++)
+            {
+                nummer[i] = random.Next(högst);
+                Console.Write(nummer[i]);
+            }
+            Swap(ref nummer[1], ref nummer[2]);
+            foreach (int element in nummer)
+                Console.Write(element + " ");
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
